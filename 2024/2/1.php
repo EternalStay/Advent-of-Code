6641e33b1e7c6d64dfcb9bcf162fa38977a4a992
EternalStay <volff.lorris@gmail.com>
2025-06-30 20:16:06 +0200

    2024 - Day 2 - Part 2

--- 2024/2/1.php
+++ 2024/2/1.php
@@ -8,29 +8,50 @@ $sum = 0;
 foreach ($strings as $string) {
     if (preg_match_all('/([0-9]+)/', $string, $matches)) {
         $match = $matches[1];
-        $matchesSort = $matches[1];
-        $matchesASort = $matches[1];
-
-        sort($matchesSort);
-        rsort($matchesASort);
-
-        if ($match == $matchesSort || $match == $matchesASort) {
+        $isOk = false;
+        
+        if (isSafe($match)) {
             $isOk = true;
-
-            for ($i = 0; $i < count($matchesSort) - 1; $i++) {
-                $diff = abs($matchesSort[$i] - $matchesSort[$i + 1]);
-                if ($diff < 1 || $diff > 3) {
-                    $isOk = false;
+            $sum++;
+        } else {
+            for ($i = 0; $i < count($match); $i++) {
+                $matchTmp = $match;
+                unset($matchTmp[$i]);
+                if (isSafe(array_values($matchTmp))) {
+                    $isOk = true;
+                    $sum++;
+                    
                     break;
                 }
-            }
-
-            if ($isOk) {
-                $sum++;
             }
         }
     }
 }
 
 echo $sum;
+
+function isSafe(array $match): bool
+{
+    $matchesSort = $match;
+    $matchesASort = $match;
+
+    sort($matchesSort);
+    rsort($matchesASort);
+
+    if ($match == $matchesSort || $match == $matchesASort) {
+        $isOk = true;
+
+        for ($i = 0; $i < count($matchesSort) - 1; $i++) {
+            $diff = abs($matchesSort[$i] - $matchesSort[$i + 1]);
+            if ($diff < 1 || $diff > 3) {
+                $isOk = false;
+                break;
+            }
+        }
+
+        return $isOk;
+    }
+
+    return false;
+}
 ?>
